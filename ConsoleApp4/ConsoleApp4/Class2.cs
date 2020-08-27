5cfc65896b8a7a6d99e2b0c1c946e5c56182b5be
Alfonso Sarmiento Ráez <asarmiento@alintia.com>
2020-08-27 11:58:11 +0200

    First

---
+++ ConsoleApp4/ConsoleApp4/Class2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    public class Class2
+    {
+        //This is a comment in the class
+        private void MethodGeniar()
+        {
+            Class1 invokacior = new Class1();
+
+            invokacior.Caca1();
+            invokacior.Caca2();
+            
+            //This is a comment in the method
+        }
+
+        private void ThisIsANewMethod()
+        { 
+            //And This is a Test
+        }
+
+        private void MethodGeniarNuevo2()
+        {
+            Class1 invokacior = new Class1();
+
+            invokacior.Caca1();
+            invokacior.Caca1("test");
+
+            Test();
+            Test();
+
+            if (1 == 1)
+            { }
+        }
+
+        private void Test()
+        { }
+    }
+}
---
+++ ConsoleApp4/ConsoleApp4/Class4.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    public class Class4
+    {
+        public int testc { get; set; }
+
+        public void Caca1()
+        {
+            //This is a defecation test
+            var a = 1;
+            a = a + 2;
+        }
+
+        public void Caca1(string a)
+        {
+            //Holi
+            //QUe ase
+
+        }
+
+        public void Caca2()
+        {
+        }
+    }
+}
